6f450d7815371dc25eef3f5ce0d59f9402afc889
Kimgigon123123 <128439525+Kimgigon123123@users.noreply.github.com>
2023-06-21 13:14:15 +0900

    ㅁㄴㅇㅁㄴㅇ
    ㅁㄴㅇㅁㄴㅇㅁㄴㅇ

---
+++ WorkSpace/And09_FragmentAdapter/app/src/main/java/com/example/and09_fragmentadapter/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.and09_fragmentadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //FrameLayout과 Button 두개 초기화 시키기.
+
+    //Button을 각각 클릭시 로그에 메뉴1,메뉴2가 출력되게 코딩해보기
+
+
+
+    FrameLayout container;
+    Button btn_menu1,btn_menu2,btn_adapter;
+    FragmentManager manager =  getSupportFragmentManager(); //프래그먼트 매니저의 초기화식.
+
+    Intent intent = getIntent();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Fragment를 붙이기 위한 공간, Fragment, FragmentManager 3가지의 요소가 필요하다.
+//        container=findViewById(R.id.container);  <= FragmentManager에서는 객체로 초기화된 위젯이 필요 x , 디자인파일에 있는 id만 명시해주면 됨
+        btn_menu1=findViewById(R.id.btn_menu1);
+        btn_menu2=findViewById(R.id.btn_menu2);
+        btn_adapter=findViewById(R.id.btn_adapter);
+
+
+
+
+        btn_menu1.setOnClickListener(this);
+        btn_menu2.setOnClickListener(this);
+        btn_adapter.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btn_menu1){
+            Log.d("확인", "onClick: 메뉴1");
+            //트랜잭션 : Oracle 작업 최소 단위 세트: commit & Rollback
+//            manager.beginTransaction().add(R.id.container,new HomeFragment(),"A").commit();
+           manager.beginTransaction().replace(R.id.container,new HomeFragment()).commit();
+
+
+        } else if (v.getId() == R.id.btn_menu2) {
+            Log.d("확인", "onClick: 메뉴2");
+            //subFragment를 하나 추가 후 (디자인 구분을 할수있게 배경색을 바꿔줌)
+            //FragmentManger를 이용해서 replace처리해보기
+            manager.beginTransaction().replace(R.id.container,new SubFragment()).commit();
+
+//            manager.beginTransaction().remove()
+
+        }else if(v.getId()==R.id.btn_adapter){
+            Intent intent = new Intent(MainActivity.this,AdapterActivity.class);
+            startActivity(intent);
+
+        }
+
+    }
+}
---
+++ WorkSpace/And09_FragmentAdapter/app/src/main/java/com/example/and09_fragmentadapter/listv/ListFragment.java
@@ -0,0 +1,45 @@
+package com.example.and09_fragmentadapter.listv;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import com.example.and09_fragmentadapter.R;
+
+import java.util.ArrayList;
+
+
+public class ListFragment extends Fragment {
+    ListView listv;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View v = inflater.inflate(R.layout.fragment_list, container, false);
+
+       ArrayList<ListDTO> list = new ArrayList<>();
+       list.add(new ListDTO(R.drawable.img1,"캐릭터1","ㅎㅇ"));
+       list.add(new ListDTO(R.drawable.img2,"캐릭터2","ㅂㅂ"));
+        list.add(new ListDTO(R.drawable.img3,"캐릭터3","ㅎㅎ"));
+        list.add(new ListDTO(R.drawable.img4,"캐릭터4","ㅋㅋ"));
+        list.add(new ListDTO(R.drawable.img5,"캐릭터5","ㅂㅇ"));
+
+
+
+
+
+       listv=v.findViewById(R.id.listv);
+        AdapterListView adapter =
+                new AdapterListView(inflater,list);
+        listv.setAdapter(adapter);
+
+       return v;
+    }
+}
